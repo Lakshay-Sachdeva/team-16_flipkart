039e454141a5cdd5e240062cec19f3fd27ed1279
jaswanth <geddamjaswanth4@gmail.com>
2021-10-02 21:47:51 +0530

    finalproject

---
+++ models/prodects.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+
+
+const products= new mongoose.Schema({
+    brand:{type:String,required:true},
+    image:{type:String,required:true},
+    rating:{type:String,required:false},
+    rom:{type:String,required:true},
+    camera:{type:String,required:true},
+    battry:{type:String,required:true},
+    procesor:{type:String,required:true},
+    warrenty:{type:String,required:true},
+    price:{type:String,required:true}
+
+
+    
+})
+const Products=mongoose.model("products",products)
+
+
+module.exports=Products
--- server.js
+++ server.js
@@ -1,60 +1,34 @@
 const express= require("express")
 const mongoose=require("mongoose")
 
+
 const app=express()
 app.use(express.json())
 //conneting data base
-
 const connect=()=>{
     return mongoose.connect("mongodb+srv://flipkartproject:flipkart@cluster0.fo2l4.mongodb.net/flipkart?retryWrites=true&w=majority")
 }
-//
 
-app.use(express.urlencoded());
-
-
-
-//model or schema data
+app.use(express.urlencoded({extended:false}));
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
-const products= new mongoose.Schema({
-    brand:{type:String,required:true},
-    iage:{type:String,required:true},
-    rating:{type:String,required:false},
-    rom:{type:String,required:true},
-    camera:{type:String,required:true},
-    battry:{type:String,required:true},
-    procesor:{type:String,required:true},
-    warrenty:{type:String,required:true},
-    price:{type:String,required:true}
+//app.use(express.static('public'))
 
 
-    
-})
-const Products=mongoose.model("products",products)
+//crud cronrolls
 
 
 
-//products controllers
-app.get("/products",async(req,res)=>{
-    const product = await Products.find({})
-    res.render("index",{product:product,
-    image:product.iage})
-    
-    })
-app.post("/products",async(req,res)=>{
-    const product = await Products.create(req.body)
-    res.status(201).send({product})
-    })
-app.patch("/products",async(req,res)=>{
-    const product = await Products.findByIdAndUpdate(req.body)
-    res.status(201).send({product})
-    })
-app.delete("/products",async(req,res)=>{
-    const product = await Products.findByIdAndUpdate(req.body)
-    res.status(201).send({product})
-    })
 
+ const product=require("./crud_controls/crudproducts")
+
+const User=require("./crud_controls/sinupcrud")
+
+const login=require("./crud_controls/logincrud")
+const crudcart=require("./crud_controls/crudcart")
+app.use("/products",product)
+app.use("/cart",crudcart)
+app.use("",User)
+app.use("",login)
 
 
 
